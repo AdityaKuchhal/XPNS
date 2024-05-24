178f891ab72a7ef9172296b1ea112b73220a2953
Aditya Kuchhal <adikuchhal@gmail.com>
2024-05-24 14:20:06 -0400

    Migration/Deployment and Ganache

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
       network_id: "*", // Match any network id
     },
   },
-  contracts_directory: "./contracts/",
+  contracts_directory: "./contracts",
   compilers: {
     solc: {
       version: "0.8.0",
