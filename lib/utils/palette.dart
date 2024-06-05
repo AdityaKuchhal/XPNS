14057fd441b9cede82ee77f3c819648a6dc7f9c7
Aditya Kuchhal <adikuchhal@gmail.com>
2024-06-04 21:28:15 -0400

    UI Completed

--- lib/utils/palette.dart
+++ lib/utils/palette.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromRGBO(166, 202, 237, 1);
 const kGreenAccent = Color(0xffddfad7);
 const kRedAccent = Color(0xfffadbd7);
+const kTextGrey = Color(0xff343f56);
